73d89b6670d940941edf72ad1ea1a6539d26efe3
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-09-17 13:25:04 -0700

    Initial Commit

---
+++ AromaThrift/AromaThrift/AromaThrift.h
@@ -0,0 +1,19 @@
+//
+//  AromaThrift.h
+//  AromaThrift
+//
+//  Created by Wellington Moreno on 9/17/16.
+//  Copyright © 2016 RedRoma, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AromaThrift.
+FOUNDATION_EXPORT double AromaThriftVersionNumber;
+
+//! Project version string for AromaThrift.
+FOUNDATION_EXPORT const unsigned char AromaThriftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AromaThrift/PublicHeader.h>
+
+
